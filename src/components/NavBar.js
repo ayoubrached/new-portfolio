73c1abba94e6302d2fe32333dc097ebda4f35b1c
Ayoub Rached <Ayoub@mac.mynetworksettings.com>
2025-03-25 23:43:11 -0400

    Fixed font and nav margins

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,9 @@ import '../App.css';
 const NavBar = () => {
     return (
         <div className='nav'>
+            <div className='navitem'>
+                Ayoub
+            </div>
             <div className='navbar'>
                 <a className='navitem' href='/'>Home</a>
                 <a className='navitem' href='/projects'>Projects</a>
